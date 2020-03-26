6e097bb49a9a7449fbd4c5563a47965c8cb59b1f
Rooted1 <ruthobey@gmail.com>
2020-03-26 14:08:07 -0500

    validate user card number

--- lib/p_readyforcheckout.rb
+++ lib/p_readyforcheckout.rb
@@ -1,10 +1,22 @@
 require_relative './p_buy_view.rb'
 require 'pry'
 
+def validate_card_number?(user_card)
+  user_card = user_card.to_s unless user_card.is_a? String
+  /\A[+-]?\d+(\.[\d]+)?\z/.match user_card
+end
+
+
 def checkout
     loop do
         puts "Please enter your 10-digit Card Number:"
-        cc_input = gets.strip
+        cc_input = nil
+        loop do
+          cc_input = gets.strip
+          break if validate_card_number?(cc_input)
+          puts "That is not a number. Pls enter only 10 digit Card Number:"
+        end
+
         if cc_input.length == 10
             current_user = User.find_by(name: $name_input)
             users_cart = Cart.find_by(user_id: current_user.id)
@@ -18,7 +30,7 @@ def checkout
             puts "Thank You for shopping Pizza Gallery, Come Again!"
             break
         else
-            puts "Invalid Card Number" 
+            puts "Invalid Card Number"
         end
     end
 end
